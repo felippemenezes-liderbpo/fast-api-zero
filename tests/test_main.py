21293246ba8987bc3c964e1a6dce765da429a3cc
Felippe Menezes <felippe.menezes@liderbpo.com.br>
2025-12-28 17:14:22 -0300

    Improve tests using bs4 to obtain more reability

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 
+from bs4 import BeautifulSoup
 from fastapi.testclient import TestClient
 
 from app.main import app
@@ -17,7 +18,9 @@ def test_root_must_return_ok_headers_json_and_message_hello_world() -> None:
 
 def test_html_must_return_ok_headers_html_and_title_hello_world() -> None:
     response = client.get('/html')
+    soup = BeautifulSoup(response.text, 'html.parser')
 
     assert response.status_code == HTTPStatus.OK
     assert response.headers['content-type'].startswith('text/html')
-    assert '<title>Hello World</title>' in response.text
+    assert soup.title is not None
+    assert soup.title.get_text(strip=True) == 'Hello World'
