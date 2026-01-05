1589b1726ccb02d107c4aad8d34270d0c73f8b9f
Felippe Menezes <felippe.menezes@liderbpo.com.br>
2026-01-05 15:11:45 -0300

    Add tests for user not-found and conflict errors

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,7 @@ from app.models import User
 from app.schemas import UserPublic
 
 
-def test_create_user(client: TestClient) -> None:
+def test_create_user(client: TestClient):
     user = {
         'username': 'alice',
         'email': 'alice@example.com',
@@ -25,7 +25,57 @@ def test_create_user(client: TestClient) -> None:
     }
 
 
-def test_read_users_with_user(client: TestClient, user: User) -> None:
+def test_create_user_with_existing_username(client: TestClient):
+    user = {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'password': 'secret',
+    }
+    response = client.post(
+        '/users/',
+        json=user,
+    )
+
+    new_user = {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'password': 'secret',
+    }
+    response = client.post(
+        '/users/',
+        json=new_user,
+    )
+
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Username already exists'}
+
+
+def test_create_user_with_existing_email(client: TestClient):
+    user = {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'password': 'secret',
+    }
+    response = client.post(
+        '/users/',
+        json=user,
+    )
+
+    new_user = {
+        'username': 'bob',
+        'email': 'alice@example.com',
+        'password': 'secret',
+    }
+    response = client.post(
+        '/users/',
+        json=new_user,
+    )
+
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Email already exists'}
+
+
+def test_read_users_with_user(client: TestClient, user: User):
     user_schema = UserPublic.model_validate(user).model_dump()
 
     response = client.get('/users/')
@@ -41,6 +91,20 @@ def test_read_users_empty(client: TestClient):
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'users': []}
+
+
+def test_read_user(client: TestClient, user: User):
+    response = client.get(f'/users/{user.id}')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == UserPublic.model_validate(user).model_dump()
+
+
+def test_read_user_not_found(client: TestClient):
+    response = client.get('/users/999')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
 
 
 def test_update_user(client: TestClient, user: User):
@@ -84,8 +148,29 @@ def test_update_integrity_error(client: TestClient, user: User):
     }
 
 
+def test_update_user_not_found(client: TestClient):
+    response = client.put(
+        '/users/999',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'newpassword',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
 def test_delete_user(client: TestClient, user: User):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
+
+
+def test_delete_user_not_found(client: TestClient):
+    response = client.delete('/users/999')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
