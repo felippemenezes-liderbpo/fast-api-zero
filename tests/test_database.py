6c9f005a852c90512787fbb167b50aabdc3bca2d
Felippe Menezes <felippe.menezes@liderbpo.com.br>
2026-01-08 11:35:39 -0300

    Remove SQLAlchemy Session type hint from test

--- tests/test_database.py
+++ tests/test_database.py
@@ -2,19 +2,19 @@ from dataclasses import asdict
 
 import pytest
 from sqlalchemy import select
-from sqlalchemy.orm import Session
 
 from app.models import User
 
 
 @pytest.mark.asyncio
-async def test_create_user(session: Session, mock_db_time):
+async def test_create_user(session, mock_db_time):
     with mock_db_time(model=User) as time:
         new_user = User(
             username='john',
             password='secret',
             email='john@example.com',
         )
+
         session.add(new_user)
         await session.commit()
 
